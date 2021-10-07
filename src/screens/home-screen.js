5236f399902210af4af70f35c1d5ba057098f7ef
Mohamed Fazaary <mohamed.fazaary@servian.com>
2021-10-07 13:21:42 +0530

    Fazaary Homescreen Widgets

--- src/screens/home-screen.js
+++ src/screens/home-screen.js
@@ -1,77 +1,239 @@
-import React, {useEffect, useState} from 'react';
-import {SafeAreaView, StatusBar, TouchableOpacity , Image, View, Text, Button} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, Dimensions, StatusBar, TouchableOpacity, Image, View, Text, Button, StyleSheet } from 'react-native';
 import { Auth, API, graphqlOperation, Analytics } from 'aws-amplify';
 import { getCoupons, getPreferences } from '../graphql/queries';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+
+
+import {
+  LineChart,
+  ProgressChart,
+} from "react-native-chart-kit";
+
+const data = {
+  labels: ["Swim"], // optional
+  data: [0.8]
+};
 
 const HomeScreen = (props) => {
   const [imageUrl, setImageUrl] = useState('');
   let [pref, setPref] = useState([]);
   useEffect(() => {
     async function fetchUserData() {
-        // You can await here
-        let data = await Auth.currentAuthenticatedUser();
-        // setUserData(data.attributes);
-        // Get current user's preferences if exists already.   
-        // const res = await API.graphql(graphqlOperation(getCoupons, {id: data.attributes.sub}));
-        const ppreferences = await API.graphql(graphqlOperation(getPreferences, {id: data.attributes.sub}));
-        // console.log(ppreferences.data.getCoupons.imgData)
-        // setImageUrl(res.data.getCoupons.imgData); 
-        setPref(JSON.parse(ppreferences.data.getPreferences.preferences))
+      // You can await here
+      let data = await Auth.currentAuthenticatedUser();
+      // setUserData(data.attributes);
+      // Get current user's preferences if exists already.   
+      // const res = await API.graphql(graphqlOperation(getCoupons, {id: data.attributes.sub}));
+      const ppreferences = await API.graphql(graphqlOperation(getPreferences, { id: data.attributes.sub }));
+      // console.log(ppreferences.data.getCoupons.imgData)
+      // setImageUrl(res.data.getCoupons.imgData); 
+      setPref(JSON.parse(ppreferences.data.getPreferences.preferences))
     }
     fetchUserData();
-}, [imageUrl]);
+  }, [imageUrl]);
 
   useEffect(() => {
-    async function sendTargetCompletionEvent() {
-      await Analytics.updateEndpoint({
-        address: 'b61e7b9465d63955320133feb0fba6aac4b567c68f5a49464e250f6c8064d4b0', // could be a device token, email address, or mobile phone number.
-        attributes: {
-            // Custom attributes that your app reports to Amazon Pinpoint. You can use these attributes as selection criteria when you create a segment.
-            hobbies: [...pref],
-            voucher_claim: ['not_claimed'],
-            status: ['target_complete']
-            
-        },
-        channelType: 'APNS', // The channel type. Valid values: APNS, GCM
-        /** Indicates whether a user has opted out of receiving messages with one of the following values:
-            * ALL - User has opted out of all messages.
-            * NONE - Users has not opted out and receives all messages.
-            */
-        optOut: 'NONE',
-        // Customized userId
-        // User attributes
-        userId: "johnmclee",
-        userAttributes: {
-            interests: [...pref],
-            // ...
-        },
-            // Buffer settings used for reporting analytics events.
-            // OPTIONAL - The buffer size for events in number of items.
-            bufferSize: 1000,
 
-            // OPTIONAL - The interval in milliseconds to perform a buffer check and flush if necessary.
-            flushInterval: 5000, // 5s 
-
-            // OPTIONAL - The number of events to be deleted from the buffer when flushed.
-            flushSize: 100,
-
-            // OPTIONAL - The limit for failed recording retries.
-            resendLimit: 5
-  
-    }).then(async () => {
-      await Analytics.record({ name: "target_done", attributes: { id: 'bicboi' } })
-      console.log('Event sent!^^')
-    }).catch(e => console.log(e));
-
-    }
-
-    sendTargetCompletionEvent();
   });
   return (
     <>
       <View>
-      </View>
+        {/* Circle Maaann Maaaannnn */}
+        <Icon
+          style={styles.iconSpacing}
+          name={'walking'} size={30} color="#000000" />
+        <Text style={
+          styles.textHeader
+        }>
+          You walked <Text style={styles.highlight}>XXXXX</Text> steps today
+        </Text>
+
+        {/* X is a State var */}
+
+        <Text style={
+          styles.infoTip
+        }>
+          X % of daily goal
+        </Text>
+
+        {/* Show Distance Walked */}
+
+        <Text style={
+          styles.distanceWalkedKM
+        }>
+          {'X'}
+        </Text>
+
+        <Text style={
+          styles.distanceWalkedSupportText
+        }>
+          {'Distance covered (kms)'}
+        </Text>
+
+        {/* Calories or any other measurement - Hardcode if not available */}
+
+        <Text style={
+          styles.calories
+        }>
+          {'X'}
+        </Text>
+
+        <Text style={
+          styles.caloriesSupportText
+        }>
+          {'Cal Burned'}
+        </Text>
+    
+        <View>
+          <ProgressChart
+            data={data}
+            width={Dimensions.get("window").width} // Code your Screen Width
+            height={300}
+            strokeWidth={6}
+            radius={60}
+            chartConfig={
+              {
+                backgroundColor: "#F4F6FA",
+                backgroundGradientFrom: "#D3D3D3",
+                backgroundGradientTo: "#F4F6FA",
+                decimalPlaces: 2, // optional, defaults to 2dp
+                color: (opacity = 1) => `rgb(104, 116, 224, ${opacity})`,
+                labelColor: (opacity = 1) => `rgba(0, 0, 139, ${opacity})`,
+                style: {
+                  borderRadius: 16,
+                  zIndex: 1
+                },
+                propsForDots: {
+                  r: "6",
+                  strokeWidth: "1",
+                  stroke: "#ffa726"
+                }
+              }
+            }
+            hideLegend={false}
+          />
+
+        </View>
+
+        {/* Statistics Maaaannnn */}
+        <LineChart
+          data={{
+            labels: ["January", "February", "March", "April", "May", "June"],
+            datasets: [
+              {
+                data: [
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100
+                ]
+              }
+            ]
+          }}
+          width={Dimensions.get("window").width} // from react-native
+          height={220}
+          yAxisLabel="$"
+          yAxisSuffix="k"
+          yAxisInterval={1} // optional, defaults to 1
+          chartConfig={{
+            backgroundGradientFrom: 0,
+            backgroundGradientFromOpacity:0,
+            backgroundGradientTo: 0,
+            backgroundGradientToOpacity: 0,
+            decimalPlaces: 2, // optional, defaults to 2dp
+            color: (opacity = 1) => `rgba(244, 175, 58, ${opacity})`,
+            labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            backgroundColor: (opacity = 0) => `rgba(255, 255, 255, ${opacity})`,
+            style: {
+              borderRadius: 16
+            },
+            propsForDots: {
+              r: "4",
+              strokeWidth: "2",
+              stroke: "#ffa726"
+            }
+          }}
+          bezier
+          style={{
+            marginVertical: 8,
+            borderRadius: 16,
+            marginTop: 0,
+            backgroundColor: 'transparent'
+          }}
+        />
+        </View>
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  iconSpacing: {
+    // marginRight: 5,
+    position: 'absolute',
+    left: "140px",
+    top: "110px",
+    zIndex: 99
+  },
+  textHeader: {
+    position: 'absolute',
+    top: "10px",
+    left: "40px",
+    zIndex: 9,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  infoTip: {
+    position: 'absolute',
+    top: "150px",
+    left: "130px",
+    zIndex: 9,
+    textAlign: 'center',
+    fontSize: 12,
+    width: 50,
+    fontWeight: 'bold',
+  },
+  highlight: {
+    color: "#6874E0",
+    fontWeight: 'bold'
+  },
+  distanceWalkedKM: { 
+    color: "black",
+    fontWeight: 'bold',
+    fontSize: 16,
+    position: 'absolute',
+    top: "250px",
+    left: "30px",
+    zIndex: 9,
+  },
+  distanceWalkedSupportText: {
+    color: "grey",
+    fontSize: 12,
+    position: 'absolute',
+    top: "270px",
+    left: "30px",
+    zIndex: 9,
+  },
+  calories: { 
+    color: "black",
+    fontWeight: 'bold',
+    fontSize: 16,
+    position: 'absolute',
+    top: "250px",
+    right: "30px",
+    zIndex: 9,
+  },
+  caloriesSupportText: {
+    color: "grey",
+    fontSize: 12,
+    position: 'absolute',
+    top: "270px",
+    right: "30px",
+    zIndex: 9,
+  }
+});
+
 export default HomeScreen;
